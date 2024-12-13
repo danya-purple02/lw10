a81b97154718587ab2f8d1e584e27af64652457d
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-12-13 08:02:10 +0300

    task 1 point 2 done

--- p10_1/p10_1/main.cpp
+++ p10_1/p10_1/main.cpp
@@ -1,10 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 int** create_adjacency_matrix(int v);
 int cout_matrix(int** G, int v);
+
+void BFSD(int** G, int v, int start, int* distance);
 
 void main()
 {
@@ -15,6 +18,34 @@ void main()
 
 	int** M = create_adjacency_matrix(size);
 	cout_matrix(M, size);
+
+	int start;
+	cout << "Enter the start vertex for BFS: ";
+	cin >> start;
+
+	int* distance = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		distance[i] = -1;
+	}
+
+	BFSD(M, size, start, distance);
+
+	cout << endl << endl << "  Depth of vertexes: " << endl;
+	cout << "vertex		distance" << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << "  " << i << "		  " << distance[i] << endl;
+	}
+
+	delete[] distance;
+	for (int i = 0; i < size; i++)
+	{
+		delete[] M[i];
+	}
+	delete[] M;
+
+	return;
 }
 
 int** create_adjacency_matrix(int v)
@@ -67,4 +98,27 @@ int cout_matrix(int** g, int v)
 		cout << endl;
 	}
 	return 1;
+}
+
+void BFSD(int** G, int v, int start, int* distance)
+{
+	queue<int> q;
+	q.push(start);
+	distance[start] = 0;
+
+	while (!q.empty())
+	{
+		int current = q.front();
+		q.pop();
+		cout << "Visiting vertex " << current << endl;
+
+		for (int i = 0; i < v; i++)
+		{
+			if (G[current][i] > 0 && distance[i] == -1)
+			{
+				q.push(i);
+				distance[i] = distance[current] + G[current][i];
+			}
+		}
+	}
 }
